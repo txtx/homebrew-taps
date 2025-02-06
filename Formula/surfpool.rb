892a00b2ce9941437dccf9ee3c51cb53cd632bb5
MicaiahReid <micaiahreid@gmail.com>
2025-02-06 08:51:44 -0500

    feat: add surfpool.rb

---
+++ Formula/surfpool.rb
@@ -0,0 +1,38 @@
+class Surfpool < Formula
+    desc "CLI tool for managing contract deployments on any blockchain"
+    homepage "https://github.com/txtx/surfpool"
+    license "Apache-2.0"
+    head "https://github.com/txtx/surfpool.git", branch: "main"
+
+    on_macos do
+        if Hardware::CPU.intel?            
+            url "https://github.com/txtx/surfpool/releases/download/v0.0.0/surfpool-darwin-x64.tar.gz"
+            # sha for macos_x64
+            sha256 "53056b22304ee5fcbcea23e6eaaff7f5c21c03050204895e11075beb4ba7cdc1"
+        elsif Hardware::CPU.arm?
+            url "https://github.com/txtx/surfpool/releases/download/v0.0.0/surfpool-darwin-arm64.tar.gz"
+            # sha for macos_arm64
+            sha256 "03e2a20833d91484fcaeb5448b1f2f9cc0a4f74e4e2ec50a5b21392c3f39874e"
+        end
+    end
+
+    on_linux do
+        if Hardware::CPU.intel?
+            url "https://github.com/txtx/surfpool/releases/download/v0.0.0/surfpool-linux-x64.tar.gz"
+            # sha for linux_x64
+            sha256 "0af64828fd4c3b7d75a0926425508ca32dadba9b3b06321d1c6525a8218e167e"
+        # elsif Hardware::CPU.arm?
+        #     url "https://github.com/txtx/surfpool/releases/download/v0.0.0/surfpool-linux-arm64.tar.gz"
+        #     # sha for linux_arm64
+        #     sha256 "SHA256_OF_LINUX_ARM64_BINARY"
+        end
+    end
+  
+    def install
+        bin.install "surfpool" => "surfpool"
+    end
+    
+    test do
+      assert_match pipe_output("#{bin}/surfpool --version", "n\n"), "surfpool-cli"
+    end
+  end
